7c0c3d86c388bc01a94eac93bdec7e7b958fa258
Leiv <Leiv@Leiv-PC>
2021-01-20 14:01:45 +0100

    init commit

---
+++ SE Hausaufgabe JUnit/src/main/Board.java
@@ -0,0 +1,83 @@
+package main;
+
+public class Board {
+
+	public static final String SYMBOL_O = "O";
+	public static final String SYMBOL_X = "X";
+
+	private static final String SYMBOL_NOT_SET = null;
+
+	private String[][] field;
+
+	@SuppressWarnings("unused")
+	public Board() {
+		field = new String[3][3];
+		for (String[] strings : field) {
+			for (String string : strings) {
+				string = SYMBOL_NOT_SET;
+			}
+		}
+	}
+	
+	public String[][] getField() {
+		return field;
+	}
+
+	public void setMark(String mark, int x, int y) throws InvalidPositionException {
+		if (mark.equalsIgnoreCase(SYMBOL_X) || mark.equalsIgnoreCase(SYMBOL_O)) {
+
+			if (x < 0 || x > 2 || y < 0 || y > 2) {
+				throw new InvalidPositionException("Invalid position: " + x + ", " + y);
+			}
+
+			if (field[x][y] == SYMBOL_NOT_SET) {
+				field[x][y] = mark;
+			} else {
+				throw new InvalidPositionException("This place is already used by the mark " + field[x][y]);
+			}
+
+		} else {
+			throw new InvalidPositionException("Invalid mark!");
+		}
+	}
+
+	public boolean hasWinner() {
+
+		if (checkColumn() || checkRow() || checkDiagonale())
+			return true;
+
+		return false;
+	}
+
+	private boolean checkColumn() {
+		for (int i = 0; i < 3; i++) {
+			if (field[i][0] == (field[i][1]) && field[i][1] == (field[i][2])
+					&& field[i][2] != (SYMBOL_NOT_SET))
+				return true;
+		}
+		return false;
+	}
+
+	private boolean checkRow() {
+		for (int i = 0; i < 3; i++) {
+			if (field[0][i] == (field[1][i]) && field[1][i] == (field[2][i])
+					&& field[2][i] != (SYMBOL_NOT_SET))
+				return true;
+		}
+		return false;
+	}
+
+	private boolean checkDiagonale() {
+
+		if (field[0][0] == (field[1][1]) && field[1][1] == (field[2][2])
+				&& field[1][1] != (SYMBOL_NOT_SET))
+			return true;
+
+		if (field[0][2] == (field[1][1]) && field[1][1] == (field[2][0])
+				&& field[1][1] != (SYMBOL_NOT_SET))
+			return true;
+
+		return false;
+	}
+
+}
---
+++ SE Hausaufgabe JUnit/src/test/TestBoard.java
@@ -0,0 +1,62 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.Board;
+import main.InvalidPositionException;
+
+public class TestBoard {
+
+	Board board = new Board();
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+	@Test
+	public void testSetMark() {
+		try {
+			board.setMark(Board.SYMBOL_X, 0, 0);
+
+			assertEquals("X", board.getField()[0][0].toString());
+			
+		} catch (InvalidPositionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test(expected = InvalidPositionException.class)
+	public void testException() throws InvalidPositionException {
+		board.setMark(Board.SYMBOL_O, 4, -1);
+		
+		board.setMark("Y", 0, 0);
+	}
+	
+	@Test
+	public void testHasWinner() {
+		
+		
+		assertFalse(board.hasWinner());
+		
+		try {
+			
+			board.setMark(Board.SYMBOL_X, 0, 0);
+			board.setMark(Board.SYMBOL_O, 1, 1);
+			board.setMark(Board.SYMBOL_X, 0, 1);
+			board.setMark(Board.SYMBOL_O, 2, 1);
+			board.setMark(Board.SYMBOL_X, 0, 2);
+			
+			
+			assertTrue(board.hasWinner());
+			
+		} catch (InvalidPositionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
